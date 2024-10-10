cfeac14d5313d6e68187b2fabbd5d3f2902f6e44
Лёха домашний <macarena7777777@gmail.com>
2024-10-10 16:53:33 +0300

    Подключил bootstrap

--- src/index.js
+++ src/index.js
@@ -5,23 +5,23 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const value = "Какое-то значение";
-const firstName = "Алекс";
-const lastName = "Брусьника";
-const getRandom = () => Math.floor(Math.random() * 10);
 
 root.render(
-  <div>
-    {/* <h1 style={{ color: "blue" }}>{value}</h1> */}
-    <h1 className="h1-style">{value}</h1>
-    <h2>{firstName} {lastName}</h2>
-    <ul>
-      {
-        Array.from({ length: 5 }, (_, index) =>
-          <li key={index}>Элемент {getRandom() % 2 ? "ODD" : "EVEN"}</li>)
-      }
-    </ul>
-  </div >
-);
+  <div className="conteiner col-10">
+    <h1 className="h1-style m-3">{value}</h1>
+
+    {Array.from({ length: 5 }, (_, index) =>
+      <div className="card m-3">
+        <img src={`https://picsum.photos/${200 + index}/100`} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Заголовок 1</h5>
+          <p className="card-text">Описание 1</p>
+        </div>
+      </div>
+    )}
+    <p className="m-3">&copy; Авторский права мои {new Date().getFullYear()}</p>
+  </div>
+)
 
 // const h1Element = document.createElement("h1");
 // h1Element.innerText = "Hello world!";
