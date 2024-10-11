68ba30c65d847ff353aee8b98d0d5ce0b2e9a9f3
Лёха домашний <macarena7777777@gmail.com>
2024-10-11 21:26:51 +0300

    Хуки

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
-import HomePage from './layout/HomePage';
+import HomePage from './layout/HomePage/HomePage';
+import MyComponents from './layout/Hooks/MyComponents';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
-  <HomePage />
+  <MyComponents />
+  // <HomePage />
 )
 
 // const h1Element = document.createElement("h1");
